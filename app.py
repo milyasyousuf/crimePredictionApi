55ef011b009c8fafa7173e26b7bcbd77a2dea2f4
milyasyousuf <m_ilyas@outlook.com>
2017-12-04 23:28:03 +0500

    getAllCrimes on basis of day completed

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import request
+from flask import request,jsonify
 import model
 import os
 from sklearn.externals import joblib
@@ -7,25 +7,24 @@ import pandas as pd
 
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-#loaded_model = joblib.load(APP_ROOT+"/joblib.pkl")
+loaded_model = joblib.load(APP_ROOT+"/joblib.pkl")
 
-@app.route('/postjson', methods=['POST'])
-def postJsonHandler():
-    if(request.is_json):
-        content = request.json()
-        print(content)
-        data = model.init(content)
-        return data
-    return "None json"
+#localhost:8000/getCrimeType?cDate=28%20:%207%20:%202017&cLtd=67.0689859&cLng=24.810707100000002
 
 @app.route('/getCrimeType', methods=['GET'])
-def test():
+def getCrimeType():
     crimeDate = request.args.get("cDate")
     crimeLng = request.args.get("cLng")
     crimeLtd = request.args.get("cLtd")
     df =pd.DataFrame({"crimeDate":[crimeDate],"crimeLng":[float(crimeLng)],"crimeLtd":[float(crimeLtd)]})
-    data = model.init(df,model)
-    return  crimeDate
+    crimeType = model.init(df,loaded_model)
+    return  jsonify(crimeType=str(crimeType[0]))
+
+#localhost:8000/getAllCrimes
+
+@app.route('/getAllCrimes', methods=['GET'])
+def getAllCrimes():
+    return model.getCrimesList(APP_ROOT)
 
 
 app.run(host='0.0.0.0', port=8000)
--- model.py
+++ model.py
@@ -4,24 +4,21 @@ import pandas as pd
 import numpy as np
 import json,collections
 import simplejson as json
-loaded_model = joblib.load("joblib.pkl")
+import datetime as datetime
+#loaded_model = joblib.load("joblib.pkl")
 
 #'Mobile snatching':1,'Robbery':2,'Kidnapping':3,'Blast':4,'Vehicle Snatching':5,"Sexual harassment:6,'Murder':7
-def init(data):
+def init(data,model):
     data['crimeDate'] = pd.to_datetime(data['crimeDate'], format='%d : %m : %Y').dt.dayofweek
     x = data.as_matrix()
-    print(loaded_model.predict(x))
+    return model.predict(x)
 
-    
-    #print(a.dtype())
-    #df = pd.read_json(test)
-    #print (elevations['crimeDate'])
-    #return df
+def getCrimesList(dir=None):
+    df = pd.read_csv(dir+"/final.csv")
+    today =(datetime.datetime.now().weekday())
+    df['dayofWeek'] = pd.to_datetime(df['crimeDate'], format='%d : %m : %Y').dt.dayofweek
+    return (df.loc[df['dayofWeek'] == today][['crimeLng','crimeLtd','typeOfCrime']].to_json(orient='records'))
 
-crimeDate = "28 : 7 : 2017"
-crimeLng = "67.0689859"
-crimeLtd = "24.810707100000002"
-df =pd.DataFrame({"crimeDate":[crimeDate],"crimeLng":[float(crimeLng)],"crimeLtd":[float(crimeLtd)]})
-    
 
-init(df)
+
+#getCrimesList()
